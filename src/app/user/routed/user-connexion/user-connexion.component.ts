844cee350659483a0b5742fe67cfe72dd6754028
keplerle <kepler.le@free.fr>
2018-12-06 11:50:14 +0100

    strutured project

---
+++ src/app/user/routed/user-connexion/user-connexion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-connexion',
+  templateUrl: './user-connexion.component.html',
+  styleUrls: ['./user-connexion.component.scss']
+})
+export class UserConnexionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
