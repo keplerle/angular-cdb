27d82a5c7075878f56b03fdbb6e3007ac6822d93
JonaszLeflour <jonasz.leflour@ecole.ensicaen.fr>
2018-12-10 11:38:12 +0100

    service : implemented service classes for user and computers

--- src/app/user/shared/user.service.ts
+++ src/app/user/shared/user.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { User } from 'src/app/shared/model/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private SERVER_URL = 'http://10.0.1.148:8080/computer-database/api/user';
+
+  constructor(private _httpClient: HttpClient) { }
+
+  getAllCompanies(): Observable<User[]> {
+    return this._httpClient.get<User[]>(`${this.SERVER_URL}/all`);
+  }
+
+  getCompanyById(id: number): Observable<User> {
+    return this._httpClient.get<User>(`${this.SERVER_URL}/${id}`);
+  }
+
+  addCompany(company: User): Observable<User> {
+    return this._httpClient.post<User>(this.SERVER_URL, company);
+  }
+
+  deleteCompanyById(id: number): Observable<User> {
+    return this._httpClient.delete<User>(`${this.SERVER_URL}/${id}`);
+  }
+
+  updateCompany(company: User): Observable<User> {
+    return this._httpClient.put<User>(this.SERVER_URL, company);
+  }
 }
