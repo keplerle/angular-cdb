20bf589c8c77ac4bd227a5195ba011253b5a30c1
JonaszLeflour <jonasz.leflour@ecole.ensicaen.fr>
2018-12-10 11:24:41 +0100

    added company service (todo : implement pipe)

--- src/app/company/shared/company.service.ts
+++ src/app/company/shared/company.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Company } from 'src/app/shared/model/company.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
+  private SERVER_URL = 'http://10.0.1.148:8080/computer-database/api/company';
 
-  constructor() { }
+  constructor(private _httpClient: HttpClient) { }
+
+  getAllCompanies(): Observable<Company[]> {
+    return this._httpClient.get<Company[]>(`${this.SERVER_URL}/all`);
+  }
+
+  getCompanyById(id: number): Observable<Company> {
+    return this._httpClient.get<Company>(`${this.SERVER_URL}/${id}`);
+  }
+
+  addCompany(company: Company): Observable<Company> {
+    return this._httpClient.post<Company>(this.SERVER_URL, company);
+  }
+
+  deleteCompanyById(id: number): Observable<Company> {
+    return this._httpClient.delete<Company>(`${this.SERVER_URL}/${id}`);
+  }
+
+  updateCompany(company: Company): Observable<Company> {
+    return this._httpClient.put<Company>(this.SERVER_URL, company);
+  }
+
 }
