af7cc3c24d4074f72a16769390f954b159a743ef
JonaszLeflour <jonasz.leflour@ecole.ensicaen.fr>
2018-12-11 11:48:24 +0100

    computer-list : added search feature

--- src/app/computer/routed/computer-list/computer-list.component.ts
+++ src/app/computer/routed/computer-list/computer-list.component.ts
@@ -13,6 +13,9 @@ export class ComputerListComponent implements OnInit {
   displayedColumns: string[];
   dataSource: Computer[];
 
+  private searchMode = false;
+  private searchString: string;
+
   pageEvent: PageEvent;
   length = 10;
   pageIndex = 0;
@@ -34,14 +37,48 @@ export class ComputerListComponent implements OnInit {
   }
 
   public getPage(event?: PageEvent) {
-    this._computerService.getAllComputersByPage(event.pageIndex + 1, event.pageSize).subscribe(response => {
-      this.pageIndex = event.pageIndex;
-      this.pageSize = event.pageSize;
+    if (this.searchMode) {
+      this.getPageWithSearch(this.searchString, this.pageIndex + 1, this.pageSize);
+    } else {
+      this.getPageNoSearch(event.pageIndex + 1, event.pageSize);
+    }
+    return event;
+  }
+
+  private getPageNoSearch(index: number, size: number) {
+    this._computerService.getAllComputersByPage(index, size).subscribe(response => {
+      this.pageIndex = index;
+      this.pageSize = size;
       this.dataSource = response;
     });
     this._computerService.getCountComputers().subscribe(response => {
       this.length = response;
     });
-    return event;
   }
+
+  private getPageWithSearch(search: string, index: number, size: number) {
+    this._computerService.getAllComputersByNameByPage(search, index, size).subscribe(response => {
+      this.pageIndex = index;
+      this.pageSize = size;
+      this.dataSource = response;
+      console.log( this.dataSource );
+    });
+    this._computerService.getCountComputersByName(search).subscribe(response => {
+      this.length = response;
+    });
+  }
+
+  applyFilter(filterValue: string) {
+    if (filterValue === null || filterValue === '') {
+      this.searchMode = false;
+      this.pageIndex = 0;
+      this.getPageNoSearch(this.pageIndex + 1, this.pageSize);
+    } else {
+      this.searchMode = true;
+      this.searchString = filterValue;
+      this.pageIndex = 0;
+      this.getPageWithSearch(this.searchString, this.pageIndex + 1, this.pageSize);
+    }
+  }
+
 }
