0ee2f95cd2d884f8aabe658eedaacd4d84c00c7e
JonaszLeflour <jonasz.leflour@ecole.ensicaen.fr>
2018-12-11 10:50:03 +0100

    Added paginator element

--- src/app/computer/routed/computer-list/computer-list.component.ts
+++ src/app/computer/routed/computer-list/computer-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ComputerService } from '../../shared/computer.service';
 import { Computer } from 'src/app/shared/model/computer.model';
+import { PageEvent } from '@angular/material';
 
 @Component({
   selector: 'app-computer-list',
@@ -11,33 +12,54 @@ export class ComputerListComponent implements OnInit {
 
   displayedColumns: string[];
   dataSource: Computer[];
+
+  pageEvent: PageEvent;
+  length = 10;
+  pageIndex = 0;
+  pageSize = 10;
+
   constructor(private _computerService: ComputerService) {
     this.displayedColumns = ['name', 'introduced', 'discontinued', 'company'];
    }
 
-   private page: number;
-   private pageSize: number;
+
 
   ngOnInit() {
-    this.page = 1;
-    this.pageSize = 10;
-    this._computerService.getAllComputersByPage(this.page, this.pageSize).subscribe(response => {
+    this._computerService.getAllComputersByPage(this.pageIndex + 1, this.pageSize).subscribe(response => {
       this.dataSource = response;
+      console.log(this.dataSource);
+    });
+    this._computerService.getCountComputers().subscribe(response => {
+      this.length = response;
     });
   }
 
-  setPage(page: number) {
-    this.page = page;
-    this._computerService.getAllComputersByPage(this.page, this.pageSize).subscribe(response => {
+  public getPage(event?: PageEvent) {
+    this._computerService.getAllComputersByPage(event.pageIndex + 1, event.pageSize).subscribe(response => {
+      this.pageIndex = event.pageIndex;
+      this.pageSize = event.pageSize;
       this.dataSource = response;
     });
-  }
-
-  setPageSize(pageSize: number) {
-    this.pageSize = pageSize;
-    this._computerService.getAllComputersByPage(this.page, this.pageSize).subscribe(response => {
-      this.dataSource = response;
+    this._computerService.getCountComputers().subscribe(response => {
+      this.length = response;
     });
-  }
 
+
+    /*this.fooService.getdata(event).subscribe(
+      response =>{
+        if(response.error) {
+          // handle error
+        } else {
+          this.datasource = response.data;
+          this.pageIndex = response.pageIndex;
+          this.pageSize = response.pageSize;
+          this.length = response.length;
+        }
+      },
+      error =>{
+        // handle error
+      }
+    );*/
+    return event;
+  }
 }
