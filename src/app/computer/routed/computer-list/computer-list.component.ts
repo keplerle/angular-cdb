65e62476de15233ae01aa9478ea43d3503319ac0
JonaszLeflour <jonasz.leflour@ecole.ensicaen.fr>
2018-12-11 17:44:52 +0100

    computer-list : added selection for deletion

--- src/app/computer/routed/computer-list/computer-list.component.ts
+++ src/app/computer/routed/computer-list/computer-list.component.ts
@@ -7,6 +7,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { ComputerCreateComponent } from '../computer-create/computer-create.component';
 import { ComputerEditComponent } from '../computer-edit/computer-edit.component';
 import { DatePipe } from '@angular/common';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-computer-list',
@@ -24,6 +25,8 @@ export class ComputerListComponent implements OnInit {
   length = 10;
   pageIndex = 0;
   pageSize = 10;
+  arrayIds: string[];
+  selection = new SelectionModel<Computer>(true, []);
 
   computer: Computer;
   constructor(
@@ -32,7 +35,7 @@ export class ComputerListComponent implements OnInit {
       public dialog: MatDialog,
        iconRegistry: MatIconRegistry,
         sanitizer: DomSanitizer) {
-    this.displayedColumns = ['name', 'introduced', 'discontinued', 'company'];
+    this.displayedColumns = ['name', 'introduced', 'discontinued', 'company', 'select'];
     this.deleteFlag = false;
     iconRegistry.addSvgIcon(
       'delete',
@@ -50,6 +53,17 @@ export class ComputerListComponent implements OnInit {
 
   ngOnInit() {
     this.getPageNoSearch(this.pageIndex + 1, this.pageSize);
+  }
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.length;
+    return numSelected === numRows;
+  }
+
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.forEach(row => this.selection.select(row));
   }
 
   public getPage(event?: PageEvent) {
@@ -146,7 +160,18 @@ public refresh() {
   addComputer(newComputer: Computer) {
     this._computerService.addComputer(newComputer).subscribe(response => {
       this.refresh();
+    });
+  }
 
+  deleteComputers() {
+    this.arrayIds = [];
+    this.selection.selected.forEach(element => {
+    this.arrayIds.push('' + element.id);
+    });
+    this._computerService.deleteComputers(this.arrayIds).subscribe(response => {
+      this.refresh();
+      this.selection.clear();
+      this.deleteFlag = false;
     });
   }
 
