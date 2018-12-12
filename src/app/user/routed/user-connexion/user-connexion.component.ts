7b2045243683023ecffb076b01a533458d4da696
keplerle <kepler.le@free.fr>
2018-12-12 18:04:46 +0100

    login functionnal

--- src/app/user/routed/user-connexion/user-connexion.component.ts
+++ src/app/user/routed/user-connexion/user-connexion.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../shared/user.service';
+import { HeaderHttpService } from '../../../shared/service/header-http.service';
 import { User } from 'src/app/shared/model/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-connexion',
@@ -11,16 +13,27 @@ export class UserConnexionComponent implements OnInit {
 
   public userInfo = new User();
 
-  constructor(private _userService: UserService) { }
+  constructor(private _userService: UserService, private _headerHttpService: HeaderHttpService, private _router: Router) { }
 
   ngOnInit() {
+    if (this._headerHttpService.getHeader() != null) {
+      this._router.navigate(['/computer']);
+    }
   }
 
   public onSubmit() {
     this._userService.connect(this.userInfo.username, this.userInfo.saltedPassword )
     .subscribe(
-      result => console.log('successful login : ' + result),
-       error => console.log('login error '));
+      result => {
+        this._router.navigate(['/computer']);
+        console.log('successful login : ' + result);
+      }
+      ,
+       error => {
+         this._headerHttpService.clearHeader();
+
+         console.log('login error ');
+      });
   }
 
 }
