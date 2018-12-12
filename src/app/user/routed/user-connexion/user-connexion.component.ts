3df6555a158c341167452be9a526cb0782d1b1f5
JonaszLeflour <jonasz.leflour@ecole.ensicaen.fr>
2018-12-12 16:24:31 +0100

    login : added login ok/error feedback

--- src/app/user/routed/user-connexion/user-connexion.component.ts
+++ src/app/user/routed/user-connexion/user-connexion.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../shared/user.service';
 import { User } from 'src/app/shared/model/user.model';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-user-connexion',
@@ -11,7 +12,7 @@ export class UserConnexionComponent implements OnInit {
 
   public userInfo = new User();
 
-  constructor(private _userService: UserService) { }
+  constructor(private _userService: UserService, public snackBar: MatSnackBar) { }
 
   ngOnInit() {
   }
@@ -19,8 +20,14 @@ export class UserConnexionComponent implements OnInit {
   public onSubmit() {
     this._userService.connect(this.userInfo.username, this.userInfo.saltedPassword )
     .subscribe(
-      result => console.log('successful login : ' + result),
-       error => console.log('login error '));
+      result => this.openSnackBar('Successful login : ' + result, 'LOGIN'),
+       error => this.openSnackBar('Coulnd\'t log-in', 'ERROR'));
   }
 
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 5000,
+    });
+  }
 }
