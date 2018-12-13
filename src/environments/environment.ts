faa008c45c8b01a4a64104708be1140b42170726
JonaszLeflour <jonasz.leflour@ecole.ensicaen.fr>
2018-12-13 15:55:58 +0100

    translation : started configuration of language select

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  countryLookup: {
+    'computer-database.com': 'en',
+    'computer-database.com.fr': 'fr'
+  }
 };
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
